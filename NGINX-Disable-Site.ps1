90e1f0db94cfc4bdce8dab9c257662acdd755341
Spencer Muise <spencer@smuise.ca>
2020-11-04 21:49:21 -0400

    Check for elevation on admin-required scripts

--- NGINX-Disable-Site.ps1
+++ NGINX-Disable-Site.ps1
@@ -1,3 +1,12 @@
+# Check for elevation
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))  
+{  
+  Write-Output "This must be run from an elevated terminal, please re-reun as admin"
+  Write-Output "Press any key to continue..."
+  $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+  exit
+}
+
 $SiteName=$args[0]
 
 $FileExists = Test-Path "C:\Tools\nginx\sites\$($SiteName).conf"
